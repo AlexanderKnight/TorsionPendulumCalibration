82c9332fabe132dac4fbb20dd295fe37cd4851b7
WilliamMorse <pendy@nakroshis.phy.usm.maine.edu>
2016-06-07 17:32:27 -0400

    set angle now takes field strength args

--- BeamSearch.py
+++ BeamSearch.py
@@ -13,43 +13,62 @@ yCoil = powersupply.PowerSupply('/dev/tty.usbserial-FTBYZZIN')
 # this one stays locked at a value to keep the laser centered on the sensor
 zCoil = powersupply.PowerSupply('/dev/tty.usbserial-FTFBPHDT') # assign the correct port the the z powersupply
 
-# Coil gain calibration values:
-# X coil = 42.24 +- 0.08 uT/A
-# Y coil = 45.99 +- 0.09 uT/A
-# Z coil = 132.16 +- 0.08 uT/A
-
-xCoilGain = 42.24e-6 # T/A
-yCoilGain = 45.99e-6 # T/A
-zCoilGain = 132.16e-6 # T/A
-
-# Aproximate Earth field components:
-# x offset = 9.58 mT
-# y offset = 17.0 mT
-# z offset = -48.9 mT
-
-
+def set_field(coil, fieldValue, fieldGain):
+    """
+    Field gain is in T/A so to get Amps we need to devide the field value by the field Gain
+    """
+    current = (fieldValue/fieldGain)*1e3 # set the current to be in milliamps
+    print(current) 
+    coil.current(current)
+    return
+                                                  
+                                                  
 # put the pendulum in it's starting position
-def set_angle(angle):
+def set_angle(angle, fieldStrength):
+    '''
+    takes the desired angle in degrees and the field strength in tesla
+    sets the supplies to these values
+    
+    the maximum field strength is given by the weakest coil.
+    in our case, the x coil 42.23 uT at max powersupply current so we shan't set the 
+    fieldStrength to be greater than this value. 
+    '''
+    
+    # Coil gain calibration values:
+    # X coil = 42.24 +- 0.08 uT/A
+    # Y coil = 45.99 +- 0.09 uT/A
+    # Z coil = 132.16 +- 0.08 uT/A
+
+    xCoilGain = 42.24e-6 # T/A
+    yCoilGain = 45.99e-6 # T/A
+    zCoilGain = 132.16e-6 # T/A
+
+    # Aproximate Earth field components:
+    # x offset = 9.58 mT
+    # y offset = 17.0 mT
+    # z offset = -48.9 mT
+    
+    xFieldOffset = 9.58e-6 # T
+    yFieldOffset = 17.0e-6 # T
+    zFieldOffset = 48.9e-6 # T
     
     angleRad = np.radians(angle) # ((np.pi)/(180))*angle
     
-    currentAmplitude = 400
-    currentOffset = 401
+    xField = xFieldOffset - (fieldStrength * np.sin(angleRad))
+    yField = yFieldOffset - (fieldStrength * np.cos(angleRad))
+    print(xField, yField)
     
-    xCurrent = currentOffset + (currentAmplitude * np.sin(angleRad))
-    yCurrent = currentOffset + (currentAmplitude * np.cos(angleRad))
-
-    xCoil.current(xCurrent)
-    yCoil.current(yCurrent)
-
+    set_field(xCoil, xField, xCoilGain) # set the x field
+    set_field(yCoil, yField, yCoilGain) # set the y field
+    
     return
 
 ###################################################
 
-startAngle = 50.00
-stopAngle = 50.05
+startAngle = 0
+stopAngle = 90
 steps = 200
-
+maxField = 9e-6
 
 
 # open the ports! 
@@ -59,10 +78,10 @@ zCoil.openPort()
 
 
 # set the z coil to be at it's nominal value:
-zCoil.current(471.0)
+#zCoil.current(471.0)
 
 # reset the cois to zero position
-set_angle(startAngle)
+set_angle(startAngle, maxField) # maximum field value
 print('sleeping for 3 seconds')
 time.sleep(3)
 
@@ -72,8 +91,8 @@ time.sleep(3)
 angles = np.linspace(startAngle,stopAngle,steps)
 print('starting circle')
 for i in angles:
-    set_angle(i)
-    print(i)
+    set_angle(i, maxField)
+    #print(i)
     
     
 # wait a bit
@@ -85,7 +104,7 @@ print('other circle')
 angles = np.linspace(stopAngle,startAngle,steps)
 
 for i in angles:
-    set_angle(i)
+    set_angle(i, maxField)
     
 # close the ports    
 xCoil.closePort()
