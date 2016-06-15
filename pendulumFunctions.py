10720f0236577684440a38648930015adf37abe4
Pendy <pendy@nakroshis.phy.usm.maine.edu>
2016-06-15 14:42:44 -0400

    field gain calibration

--- pendulumFunctions.py
+++ pendulumFunctions.py
@@ -24,7 +24,7 @@ zFieldGain = u.ufloat(132.16e-6, 0.08e-6) # T/A
 xAFieldGain = xFieldGain / 25 # T/A
 yAFieldgain = yFieldGain / 20 # T/A
 
-# power supply current command limits 
+# power supply current command limits
 maxPowerSupplyCurrent = 0.9999 # A
 minPowerSupplyCurrent = 0.0010 # A
 
@@ -43,13 +43,13 @@ xAppliedMinField = xFieldGain.n * minPowerSupplyCurrent
 yAppliedMinField = yFieldGain.n * minPowerSupplyCurrent
 zAppliedMinField = zFieldGain.n * minPowerSupplyCurrent
 
-#torsionalZero = 
-#torsionConstant = 
+#torsionalZero =
+#torsionConstant =
 
 # find what quadrent we are operating in
-FIELD_QUADRENT = 1 # can be a number between 1 and 4. 
+FIELD_QUADRENT = 1 # can be a number between 1 and 4.
 
-# adust the field gains to reflect our quadrent choice. 
+# adust the field gains to reflect our quadrent choice.
 if FIELD_QUADRENT == 1:
     # dont change the gain valus
 elif FIELD_QUADRENT == 2:
@@ -64,52 +64,67 @@ elif FIELD_QUADRENT == 4:
     yFieldGain *= -1
 else:
     raise exception('bad FIELD_QUADRENT value. Should be an integer 1,2,3 or 4.')
-    
+
 
 def fine_field_cart(xField, yField):
     '''
     set the large and small coils to obtain a certin field value
     use the small coils with the labjack in command response mode.
-    
-    only adust the large coil if the small DACs are out of range. 
+
+    only adust the large coil if the small DACs are out of range.
     '''
-    # should we make a powersupply like class to handel the dacs on the labjack?
-    
+    # should we make a powersupply like class to controll the dacs on the labjack?
+
     DAC_VOLTS_PER_AMP_GAIN = 1 / u.ufloat(250.00, 0.03) # Vin/I = 1/R where R = 250.00 +- 0.03 ohms
-    DAC_VOLTS_PER_TESLA = DAC_VOLTS_PER_AMP_GAIN / aFieldGain
-    
+    # check units here should be good: ((V/A) / (T/A)) * (T) = (V)
+    xVoltage = (DAC_VOLTS_PER_AMP_GAIN / xAFieldGain) * xField
+    yVoltage = (DAC_VOLTS_PER_AMP_GAIN / yAFieldgain) * yField
+
+    # now write to the labjack with eWriteNames()
+    try:
+        names = ['DAC0', 'DAC1']
+        values = [xVoltage, yVoltage]
+        numFrames = 2 # number of values to write to the labjack
+        ljm.eWriteNames(handle, numFrames, names, values)
+        pass
+    except Exception as e:
+        raise
+
+    return
+
+
 
 def field_polar(fieldMagnitude, fieldDirection):
     '''
     sets the field in the coils using the calibration data with polar coridinates as inputs
     does not allow the field to be greater than the smallest field offset.
-    '''    
+    '''
     angleRad = np.radians(fieldDirection) # ((np.pi)/(180))*angle
-    
-    # convert to cartesian 
+
+    # convert to cartesian
     xField = xFieldOffset + (fieldMagnitude * np.sin(angleRad))
     yField = yFieldOffset + (fieldMagnitude * np.cos(angleRad))
-    
+
     # devide the field-component by the axis gain to get the desired current for each coil
     xCurrent = xField / xFieldGain
     yCurrent = yField / yFieldGain
-    
+
     xCoil.current(xCurrent.n*1e3) # set to the nominal value of the current
     yCoil.current(yCurrent.n*1e3) # with the .n (from the uncertinties package)
-    
+
     return
 
 def field_cart(xField, yField):
     '''
     sets the field in the coils using the calibration data with cartesian coordinates as input
     '''
-    
+
     # devide the field-component by the axis gain to get the desired current for each coil
     xCurrent = xField / xFieldGain
     yCurrent = yField / yFieldGain
-    
+
     print(xCurrent.n*1e3, yCurrent.n*1e3)
     xCoil.current(xCurrent.n*1e3) # set to the nominal value of the current
     yCoil.current(yCurrent.n*1e3) # with the .n (from the uncertinties package)
-    
+
     return
