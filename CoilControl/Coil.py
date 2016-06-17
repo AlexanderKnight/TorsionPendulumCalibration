a2f559e86d853da9f32ead636495e2e90ccec3fe
WilliamMorse <william.b.morse@maine.edu>
2016-06-17 15:51:46 -0400

    fine adjustment function
    fine adjustment function should work!

--- CoilControl/Coil.py
+++ CoilControl/Coil.py
@@ -18,7 +18,7 @@ class Coil:
         self.appliedMinField = fieldGain.n * minPowerSupplyCurrent
 
         # innitalize field value containers
-        self.CoilField = 0.0
+        self.coilField = 0.0 # total field
 
         return(self)
 
@@ -47,14 +47,35 @@ class CoilWithCorrection(Coil):
         # use this value to measure the smallest deveation avalable from the
         # large powersupplies
         self.minPowerSupplyCurrentStep = 0.0001 # Amps
-        # and the field?
-
+        # and the field now gets divided into two coils
+        self.largeCoilField = 0.0 # portion of the total field controlled by the large coils
+        self.smallCoilField = 0.0 # portion of the total field for the small coils
 
         return(self)
 
 
-class ClassName(object):
-    """docstring for """
-    def __init__(self, arg):
-        super(, self).__init__()
-        self.arg = arg
+    def setField(self, fieldValue):
+
+        # calculate the smallest field that the large coil can produce
+        minimumLargeCoilFieldStep = self.minPowerSupplyCurrentStep * self.LargeFieldGain
+        # total range of the small coil.
+        # |--|--|--|--|--|--|--|--| the small ticks are the minimumLargeCoilFieldStep
+        #    |--|--|**|--|--|       this is the range of the dac after voltage clamping band
+        #  pick the ** for the middle of our field.
+        # usable +- range of the small coil (total range is 3 steps)
+        #    |--{--|**|--}--|       Curly braces are the trigger points where we want to renormalize
+        smallCoilFieldRange = 1.5 * minimumLargeCoilFieldStep
+        # |  |--|--|**|--|--|       Distance from the left side is 3.5 smallest divisions
+        largeCoilFieldOffset = minimumLargeCoilFieldStep * 3.5
+        # the extra .5 above is to hack the rounding in the current function :P
+        smallCoilFieldOffse = minimumLargeCoilFieldStep * 3.0
+        # with this in mind let's split the field btween the large and small coils
+        # the large coil is easy we just subtract the field offset and let the
+        # powersupply.PowerSupply.current() function round up or down with format
+        self.largeCoilField = (fieldValue - largeCoilFieldOffset)
+        # for the small coil, we need to first provide the field offset but also
+        # calculate the remaining field (with mod) to get a precise measurement
+        smallCoilFieldRemainder = (fieldValue % minimumLargeCoilFieldStep)
+        # add the offset with the remainder to get the small field value.
+        self.smallCoilField = smallCoilFieldOffset + smallCoilFieldRemainder
+        self.setLargeCoilField(fieldValue - largeCoilFieldOffset)
