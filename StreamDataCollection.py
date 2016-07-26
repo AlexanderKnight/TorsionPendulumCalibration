61d9a9b00ee25c0c24b882355497a8522b9c1684
Pendy <pendy@nakroshis.phy.usm.maine.edu>
2016-07-26 13:35:42 -0400

    lab jack streaming stuff

--- StreamDataCollection.py
+++ StreamDataCollection.py
@@ -6,10 +6,13 @@ from labjack import ljm
 import time
 import sys
 from datetime import datetime
+import numpy as np
 
 MAX_REQUESTS = 50 # The number of eStreamRead calls that will be performed.
 FIRST_AIN_CHANNEL = 0 #AIN0
 NUMBER_OF_AINS = 3 # AIN0: L-R, AIN1: Sum, AIN2: T-B
+
+rawData = [0,0,0]
 
 # open the all ports and get the labjack handle
 handle = xyz.openPorts()
@@ -29,19 +32,89 @@ scanRate = 1000
 scansPerRead = int(scanRate/2)
 
 try:
-    # all labjack code in this try/except you could put all your code here!
+    # Configure the analog inputs' negative channel, range, settling time and
+    # resolution.
+    # Note when streaming, negative channels and ranges can be configured for
+    # individual analog inputs, but the stream has only one settling time and
+    # resolution.
+    aNames = ["AIN_ALL_NEGATIVE_CH", "AIN_ALL_RANGE", "STREAM_SETTLING_US",
+              "STREAM_RESOLUTION_INDEX"]
+    aValues = [ljm.constants.GND, 10.0, 0, 0] #single-ended, +/-10V, 0 (default),
+                                              #0 (default)
+    ljm.eWriteNames(handle, len(aNames), aNames, aValues)
+
+
     while True:
-        print("hi")
+        # Configure and start stream
+        scanRate = ljm.eStreamStart(handle, scansPerRead, numAddresses, aScanList, scanRate)
+        print("\nStream started with a scan rate of %0.0f Hz." % scanRate)
+
+        print("\nPerforming %i stream reads." % MAX_REQUESTS)
+        start = datetime.now()
+        totScans = 0
+        totSkip = 0 # Total skipped samples
+
+        i = 1 # counter for number of stream requests
+        while i <= MAX_REQUESTS:
+            ret = ljm.eStreamRead(handle)
+
+            data = ret[0]
+            scans = len(data)/numAddresses
+            totScans += scans
+
+            # Count the skipped samples which are indicated by -9999 values. Missed
+            # samples occur after a device's stream buffer overflows and are
+            # reported after auto-recover mode ends.
+            curSkip = data.count(-9999.0)
+            totSkip += curSkip
+
+            print("\neStreamRead %i" % i)
+            ainStr = ""
+            for j in range(0, numAddresses):
+                ainStr += "%s = %0.5f " % (aScanListNames[j], data[j])
+            print("  1st scan out of %i: %s" % (scans, ainStr))
+            print("  Scans Skipped = %0.0f, Scan Backlogs: Device = %i, LJM = " \
+                  "%i" % (curSkip/numAddresses, ret[1], ret[2]))
+            # append the data to the data List
+
+            rawData = np.reshape(data, (-1,NUMBER_OF_AINS)) # reshape the data to have each row be a different reading
+            print(rawData,'\n')
+
+            i += 1
+
+
+        end = datetime.now()
+
+        print("\nTotal scans = %i" % (totScans))
+        tt = (end-start).seconds + float((end-start).microseconds)/1000000
+        print("Time taken = %f seconds" % (tt))
+        print("LJM Scan Rate = %f scans/second" % (scanRate))
+        print("Timed Scan Rate = %f scans/second" % (totScans/tt))
+        print("Timed Sample Rate = %f samples/second" % (totScans*numAddresses/tt))
+        print("Skipped scans = %0.0f" % (totSkip/numAddresses))
+
+        print("\nStop Stream")
+        ljm.eStreamStop(handle)
+
+        input("press enter to start a new scan")
+
+except ljm.LJMError:
+    ljme = sys.exc_info()[1]
+    print(ljme)
+    #xyz.closePorts(handle)
+
 except KeyboardInterrupt: # usefull to have a KeyboardInterrupt when your're debugging
-    xyz.closePorts(handle)
+    #xyz.closePorts(handle)
+    # save the data to a DataFrame
+    print(rawData)
 
 except Exception as e:
-	# helpful to close the ports on except when debugging the code.
+    # helpful to close the ports on except when debugging the code.
     # it prevents the devices from thinking they are still conected and refusing the new connecton
     # on the next open ports call.
-	xyz.closePorts(handle)
-	print('closed all the ports\n')
-	print(e) # print the exception
+    xyz.closePorts(handle)
+    print('closed all the ports\n')
+    print(e) # print the exception
     raise
 
 xyz.closePorts(handle)
