eb41814b0ebc20a54f740a6ea2f6f217a9112e27
Alexander Knight <alexander.knighr@maine.edu>
2016-08-01 08:42:12 -0400

    changes

--- PeriodCalculations/period.py
+++ PeriodCalculations/period.py
@@ -1,21 +1,22 @@
 import numpy as np
 import pandas as pd
 
-def periodCalc (data, sumCrop=4.5, swingCrop=3.0):
+
+def periodCalc (data, sumCrop=4.5, swingCrop=4.5):
 
     eventNumbers = data.eventNumber.unique() # checks for unique event numbers
 
     periodList=[] #creates an array that will be appended to
-    for l in eventnumbers: #goes through data by event number
+    for l in eventNumbers: #goes through data by event number
         selectedData = data[data.eventNumber == l] #only checks event number data
 
         #crops end where data is bad
-        for i in reversed(selectedData.leftMinusRight):
-            if selectedData.leftMinusRight[i] >= swingCrop
+        for i in reversed(selectedData.leftMinusRight.index):
+            if selectedData.leftMinusRight[i] >= swingCrop \
             or selectedData.leftMinusRight[i] <= -1*swingCrop:
                 swingCropIndex = i
                 break
-        selectedData = slectedData[selectedData.index <= swingCropIndex]
+        selectedData = selectedData[selectedData.index <= swingCropIndex]
 
         crossingsIndex=[]
         #checks to see if the sign from one element to the next changes, and then
@@ -30,11 +31,15 @@ def periodCalc (data, sumCrop=4.5, swingCrop=3.0):
 
         #creates a boolean array to append to data DataFrame to show crossings
         crossings = np.zeros(len(selectedData.index))
+        print(np.shape(selectedData.index))
+        print(np.shape(crossings))
         for i in crossingsIndex:
             crossings[i] = True
-        data.crossings = crossings
+        selectedData['crossings'] = pd.Series(crossings, index=selectedData.index)
         #crops data to only include crossings and high sum signal
-        newdata = selectedData[selectedData.sumSignal >= sumCrop and selectedData.crossings]
+        newdata = selectedData.loc[selectedData['sumSignal'] >= sumCrop]
+        newdata = newdata.loc[newdata['crossings'] == True]
+
 
         #resets index
         newdata = newdata.reset_index()
@@ -45,3 +50,6 @@ def periodCalc (data, sumCrop=4.5, swingCrop=3.0):
         avgPeriod = np.mean(periods)
         periodList.append([l, avgPeriod, data.xField.mean(), data.yField.mean()])
     return periodList
+
+Data = pd.read_csv('freqVsField16-07-27~13-04-55.csv')
+print(periodCalc(Data))
